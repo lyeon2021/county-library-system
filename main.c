36997409ecd4364935fe99fd452dbdd964980a1d
lyeon2021 <87859880+lyeon2021@users.noreply.github.com>
2021-07-23 19:08:49 +0300

    implemented exit action

--- main.c
+++ main.c
@@ -1,17 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<windows.h>
 /****************
 lyeon ndegwa
 community lab project
 23/6/2021
 **********************/
+
+
+// Function prototype
+
+void execute_action(int action);
+void close();
+int menu();
+
 int main()
 {
+
     int action;
 
-    printf("\tcounty library management system!\n");
+    printf("\t county library management system!\n");
     printf("welcome lyeon\n");
-    //Navigation menu
+
+    action = menu();
+    execute_action(action);
+
+    return 0;
+}
+void execute_action(int action) {
+
+switch(action) {
+
+case 1:
+    printf("Adding a user..\n");
+    break;
+
+case 2:
+    printf("Adding a book..\n");
+    break;
+
+case 3:
+    close();
+    break;
+
+default:
+    printf("Unknown function.\n");
+}
+
+
+}
+
+
+void close(){
+
+printf("Thank you for using the system\n");
+
+printf("Bye\n");
+Sleep(5000);
+
+exit(0);
+
+}
+
+
+int menu (){
+
+ //Navigation menu
+ int action;
     do {
     printf("1. Add user\n");
     printf("2. Add book\n");
@@ -20,16 +75,14 @@ int main()
     scanf("%d",&action);
 
     //Validate input
-    if (action < 1 || action > 3)
-    printf("Invalid action.Try again\n");
+    if (action < 1 || action > 3){
+        printf("Invalid action.Try again\n");
+        Sleep(2000);
+        system( "cls");
+    }
 
     }while(action < 1 || action > 3);
 
+    return action;
 
-
-
-
-
-
-    return 0;
 }
